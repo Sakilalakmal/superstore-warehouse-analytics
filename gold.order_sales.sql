648204a08bd9b447f149925b3df65db1b67d22dc
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2026-03-16 11:02:12 +0530

    Add gold schema DDL for fact_order_sales and dim_products views

---
+++ gold.order_sales.sql
@@ -0,0 +1,40 @@
+-- use superstore_warehouse_analytics
+USE superstore_warehouse_analytics;
+GO
+-- create view
+
+CREATE OR ALTER VIEW gold.fact_order_sales AS
+SELECT
+	order_id,
+	product_id,
+	customer_name,
+	ship_mode,
+	segment,
+	state,
+	country,
+	market,
+	region,
+	sales,
+	quantity,
+	discount,
+	profit,
+	shipping_cost,
+	order_priority,
+	order_date,
+	ship_date,
+	year,
+	DATEDIFF(day,order_date,ship_date) AS shipping_days,
+	CASE WHEN profit > 0 THEN 1 ELSE 0 END is_profitable,
+	CASE WHEN order_priority IN ('Critical','High') THEN 1 ELSE 0 END is_high_priority
+FROM silver.order_sales;
+
+
+-- gold.products table
+
+CREATE OR ALTER VIEW gold.dim_products AS 
+SELECT
+	product_id,
+	category,
+	sub_category,
+	product_name
+FROM silver.products;
